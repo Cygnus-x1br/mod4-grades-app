336b1db744c2d0b2c643905443c14359fc3d3be0
Cygnus-x1br <jeanmarcel83@gmail.com>
2020-07-09 14:01:09 -0300

    Ajuste de deploy

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'https://quiet-anchorage-94951.herokuapp.com/',
+  baseURL: 'https://shielded-inlet-85418.herokuapp.com',
   headers: {
     'Content-type': 'application/json',
   },
